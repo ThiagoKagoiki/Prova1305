9ddbdc18c9d3c22789418afdf52f78e4dae66425
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-05-13 15:41:14 -0300

    colocando o js em funcao

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const fs = require("fs")
 
 
-const crypto = require("crypto");
-const { geradorNome } = require("gerador-nome");
-let uuid = crypto.randomUUID() //id aleatorio
 
-const data = new Date();
-const dia = data.getDate();
-const mes = data.getMonth();
-const year = data.getFullYear();
-const hora = data.getHours();
-const minutos = data.getMinutes();
-const segundos = data.getSeconds();
+const salvarLogs = () => {
+    const fs = require("fs")
+    const crypto = require("crypto");
+    const { geradorNome } = require("gerador-nome");
+    let uuid = crypto.randomUUID() //id aleatorio
 
-fs.writeFileSync("logs.txt", uuid + ' - ' + year + '-' + mes + '-' + dia + ' - ' + hora + ':' + minutos + ':' + segundos + ' - '+ geradorNome() + '\n', {flag: 'a'})
+    const data = new Date();
+    const dia = data.getDate();
+    const mes = data.getMonth();
+    const year = data.getFullYear();
+    const hora = data.getHours();
+    const minutos = data.getMinutes();
+    const segundos = data.getSeconds();
+
+    fs.writeFileSync("logs.txt", uuid + ' - ' + year + '-' + mes + '-' + dia + ' - ' + hora + ':' + minutos + ':' + segundos + ' - '+ geradorNome() + '\n', {flag: 'a'})
+}
+
+salvarLogs()
 
 
