32df843149e0be55f7cbb9bd5e41739481532a09
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-05-13 16:11:34 -0300

    porta 3000 funcionando e passando para o logs.txt

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
+const fs = require("fs")
+const crypto = require("crypto");
+const express = require("express")
+const app = express()
 
 
+app.use(express.json())
 
-const salvarLogs = () => {
-    const fs = require("fs")
-    const crypto = require("crypto");
-    const { geradorNome } = require("gerador-nome");
+
+const salvarLogs = (nome) => {
+    
     let uuid = crypto.randomUUID() //id aleatorio
 
     const data = new Date();
     const dia = data.getDate();
-    const mes = data.getMonth();
+    const mes = data.getMonth() + 1;
     const year = data.getFullYear();
     const hora = data.getHours();
     const minutos = data.getMinutes();
     const segundos = data.getSeconds();
 
-    fs.writeFileSync("logs.txt", uuid + ' - ' + year + '-' + mes + '-' + dia + ' - ' + hora + ':' + minutos + ':' + segundos + ' - '+ geradorNome() + '\n', {flag: 'a'})
+    const fswrite = (uuid + ' - ' + year + '-' + mes + '-' + dia + ' - ' + hora + ':' + minutos + ':' + segundos + ' - '+ nome + '\n')
+
+    fs.writeFileSync("logs.txt", fswrite, {flag: 'a'})
+
+    return uuid
 }
 
-salvarLogs()
+app.post("/logs", (req, res) => {
+    const {nome} = req.body
+    if(!nome){
+        return res.status(400).json({erro: "Nome é obrigatório"})
+    }
+    const id = salvarLogs(nome)
 
+    res.status(200).json({
+        id: id,
+        msg: "Aluno registrado com sucesso"
+    })
+})
 
+app.get("/logs", (req, res) => {
+    try{
+        const dados = fs.readFileSync("logs.txt", "utf-8")
+        res.status(200).send(dados)
+    }catch(erro){
+        res.status(500).json({erro: "Erro ao ler os logs"})
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
